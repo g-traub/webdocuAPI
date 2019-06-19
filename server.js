d2bdb5fd654c886f79cab817261d019972924689
Guillaume <guillaume.trb@hotmail.com>
2019-06-19 22:31:10 +0200

    deleted unecessary files

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ Importer les composants serveur
     //Class
     require('dotenv').config();
     const express = require('express');
-    const ejs = require('ejs');
     const path = require('path');
     const bodyParser = require('body-parser');
     
     //Modules serveur
-    const frontRoutes = require('./routes/front.routes');
     const apiRoutes = require('./routes/api.routes');
 //
 
@@ -21,21 +19,12 @@ Configuration du serveur
     const server = express();
     const port  = process.env.PORT;
     
-
-    // Définition du dossier static du client
-    server.set( 'views', __dirname + '/www' );
-    server.use( express.static(path.join(__dirname, 'www')) );
-
-    // Configuration du moteur de rendu
-    server.set( 'view engine', 'ejs' );
-
     //Configuration de body-parser
     server.use(bodyParser.json({limit: '10mb'}));
     server.use(bodyParser.urlencoded({ extended: true }));
 
     //Utilisation des routeurs 
     server.use('/api', apiRoutes);
-    server.use( '/', frontRoutes);
 //
 
 /* 
